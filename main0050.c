c16e47e765f1bfa2dd4c465c576bb48e3b4913d2
wangsir9825 <74584256+wangsir9825@users.noreply.github.com>
2020-12-19 08:04:55 +0800

    Update main0050.c
    程序功能：考虑多行输入，对于每次输入的值只需判断是否大于140
    链接：https://www.nowcoder.com/practice/557cc54704bb4d56b73b62d1a5455331?tpId=107&&tqId=33322&rp=1&ru=/ta/beginner-programmers&qru=/ta/beginner-programmers/question-ranking
    时间：2020年12月18日22:21:58
    题目描述
    	据说智商140以上者称为天才，KiKi想知道他自己是不是天才，请帮他编程判断。输入一个整数
    	表示一个人的智商，如果大于等于140，则表明他是一个天才，输出“Genius”。
    输入描述:
    	多组输入，每行输入包括一个整数表示的智商。
    输出描述:
    	针对每行输入，输出“Genius”。
    示例1
    	输入
    		160
    	输出
    		Genius

--- main0050.c
+++ main0050.c
@@ -1,33 +1,15 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//12345 1 2345	12  345		123 45		1234 5
+
 int main()
 {
-	int i = 0;
-	for (i = 10000; i <= 99999; i++)
+	int IQ = 0;
+	while (scanf("%d", &IQ) != EOF)
 	{
-		if (((i / 10000) * (i % 10000) + (i / 1000) * (i % 1000) + (i / 100) * (i % 100) + (i / 10) * (i % 10)) == i)
-			printf("%d ", i);
-	}
-	return 0;
-}
-/*
-int main() {
-
-	//遍历所有的五位数，从10000开始
-	for(int i = 10000; i <= 99999; i++) {
-		//设置当前和，除数
-		int sum = 0, mod = 10000, cur = i;
-		//循环直到mod/10为0
-		while(mod / 10) {
-			//根据公式计算当前的除数和余数，进行累计求和
-			sum += (cur % mod) * (cur / mod);
-			//更新mod
-			mod /= 10;
+		if (IQ >= 140)
+		{
+			printf("Genius\n");
 		}
-		//判断是否满足水仙花数
-		if(i == sum)
-			printf("%d ", i);
 	}
 	return 0;
 }
