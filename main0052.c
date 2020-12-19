3cd3d642b84f5a1dd4ddf27927c1c40e8b5058c4
wangsir9825 <74584256+wangsir9825@users.noreply.github.com>
2020-12-19 08:08:46 +0800

    程序功能：多组输入，判断每次的分数是否及格
    链接：https://www.nowcoder.com/practice/56513524333148b38945e1989bc7df4e?tpId=107&tags=&title=&diffculty=0&judgeStatus=0&rp=1
    时间：2020年12月18日22:34:28
    题目描述
    	KiKi想知道他的考试分数是否通过，请帮他判断。从键盘任意输入一个整数表示的分数，编程判断该分
    	数是否在及格范围内，如果及格，即：分数大于等于60分，是输出“Pass”，否则，输出“Fail”。
    输入描述:
    	多组输入，每行输入包括一个整数表示的分数（0~100）。
    输出描述:
    	针对每行输入，输出“Pass”或“Fail”。
    示例1
    	输入
    		94
    	输出
    		Pass
    示例2
    	输入
    		44
    	输出
    		Fail

--- main0052.c
+++ main0052.c
@@ -3,38 +3,16 @@
 
 int main()
 {
-	char arr[20];
-	int i = 0;
-	int a = 0;
-	int b = 0;
-	scanf("%s", arr);
-	while (arr[i] != '0')
+	int grade = 0;
+	while (scanf("%d", &grade) != EOF)
 	{
-		if (arr[i] == 'A')
-			a++;
-		if (arr[i] == 'B')
-			b++;
-		i++;
+		if (grade >= 60 && grade <= 100)
+		{
+			printf("Pass\n");
+		}
+		else
+			printf("Fail\n");
 	}
-	if (a > b)
-		printf("A");
-	else if (a < b)
-		printf("B");
-	else
-		printf("E");
+
 	return 0;
 }
-/*
-	char ch = 0;
-	//循环读取投票
-	while(scanf("%c", &ch) != EOF) {
-		//当前字符为0则退出
-		if(ch == '0')
-			break;
-		//统计a的票数，同理也要统计b的票数
-		if(ch == 'A')
-			a_count++;
-		if(ch == 'B')
-			b_count++;
-	}
-*/
