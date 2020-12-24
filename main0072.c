2a0cece5e838afc04bba17c4f4568e7c7f30d853
wangsir9825 <wangsir9825@163.com>
2020-12-24 22:34:28 +0800

    程序功能：打印用“*”组成的翻转金字塔图案 链接：https://www.nowcoder.com/practice/c4775776e4464537bfb6a5ba37c596c6?tpId=107&tags=&title=&diffculty=0&judgeStatus=0&rp=1 时间：2020年12月24日22:07:21 题目描述 	KiKi学习了循环，BoBo老师给他出了一系列打印图案的练习，该任务是打印用“*”组成的翻转金字塔图案。 输入描述: 	多组输入，一个整数（2~20），表示翻转金字塔边的长度，即“*”的数量，也表示输出行数。 输出描述: 	针对每行输入，输出用“*”组成的金字塔，每个“*”后面有一个空格。 示例1 	输入 		5 	输出 		* * * * * 		 * * * * 		  * * * 		   * * 			* 示例2 	输入 		6 	输出 		* * * * * * 		 * * * * * 		  * * * * 		   * * * 			* * 			 *

---
+++ main0072.c
@@ -0,0 +1,39 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int n = 0;
+	int m = 0;
+	int flag = 1;
+	while (scanf("%d", &n) != EOF)
+	{
+		int i = 0, j = 0;
+		m = 2 * n - 1;
+		for (j = 0; j < n; j++)
+		{
+			for (i = 0; i < m; i++)
+			{
+				if (i < j)
+					printf(" ");
+				else
+				{
+					if (flag == 1)
+					{
+						printf("*");
+						flag = 0;
+					}
+					else
+					{
+						printf(" ");
+						flag = 1;
+					}
+				}
+			}
+			m--;
+			flag = 1;
+			printf(" \n");
+		}
+	}
+	return 0;
+}
