dfe284d4cb9be67fe4e004a0ccf03e90bb12f368
wangsir9825 <wangsir9825@163.com>
2020-12-25 15:15:04 +0800

    程序功能：打印用“*”组成的正斜线形图案 链接：https://www.nowcoder.com/practice/61ef68d129534dfbb04b232e1244e447?tpId=107&tags=&title=&diffculty=0&judgeStatus=0&rp=1 时间：2020年12月25日08:48:49 题目描述 	KiKi学习了循环，BoBo老师给他出了一系列打印图案的练习，该任务是打印用“*”组成的正斜线形图案。 输入描述: 	多组输入，一个整数（2~20），表示输出的行数，也表示组成正斜线的“*”的数量。 输出描述: 	针对每行输入，输出用“*”组成的正斜线。 示例1 	输入 		4 	输出 		   * 		  * 		 * 		*

---
+++ main0077.c
@@ -0,0 +1,23 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int n, i, j;
+	while (scanf("%d", &n) != EOF)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == n-i-1)
+					printf("*");
+				else
+					printf(" ");
+
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
