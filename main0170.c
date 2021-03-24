8a7541737df40015ca084fe8cd176369fd56dd4c
wangsir9825 <wangsir9825@163.com>
2021-03-24 22:18:29 +0800

    程序功能：nowcoder 输出 n行"Happy new year!Good luck!"。 链接：https://www.nowcoder.com/practice/659248911f4f4a60bcc7e0892f91c268?tpId=107&tags=&title=&diffculty=0&judgeStatus=0&rp=1&tab=answerKey 时间：2021年3月24日22:15:16 题目描述 	2020年来到了，KiKi要对他的n位好朋友说n遍”Happy new year!Good luck!”的祝福语。 输入描述: 	输入只有一行，其中含有一个正整数 n(1≤n≤20)，代表KiKi说祝福话的遍数。 输出描述: 	输出 n行"Happy new year!Good luck!"。 示例1 	输入 		2 	输出 		Happy new year!Good luck! 		Happy new year!Good luck!

---
+++ main0170.c
@@ -0,0 +1,14 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int i = 0;
+	for (i = 0; i < n; ++i)
+	{
+		printf("Happy new year!Good luck!\n");
+	}
+	return 0;
+}
